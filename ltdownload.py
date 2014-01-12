90f62b083e596b8f45778318ba92483e0b4336cb
Juhnson <Juhnson.Guo@gmail.com>
2014-01-12 16:48:11 +0800

    Update ltdownload.py

--- ltdownload.py
+++ ltdownload.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  未命名.py
+#  ltdowndoad.py
 #  
-#  Copyright 2014 Juhnson <juhnson@juhnson-desktop>
+#  Copyright 2014 Juhnson <juhnson.Guo@Gmail.com>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
